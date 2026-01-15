7bfb54d6f1fdcefd0bc8ff07fba068553ecf9b30
Brandon Peters <brandon.peters@gmail.com>
2026-01-15 01:22:47 -0500

    Update

--- install/calibre-install.sh
+++ install/calibre-install.sh
@@ -21,10 +21,13 @@ msg_info "Installing Calibre"
 mkdir -p /var/lib/calibre/
 chmod 775 /var/lib/calibre/
 cd /var/lib/calibre/
-$STD curl -fsSL 'https://download.calibre-ebook.com/linux-installer.py' -o linux-installer.py
-$STD python3 linux-installer.py
-rm -rf linux-installer.py
-mkdir -p /var/lib/calibre/library
+$STD curl -fsSL 'https://download.calibre-ebook.com/linux-installer.sh' -o linux-installer.sh
+chmod +x linux-installer.sh
+$STD ./linux-installer.sh install_dir=/opt
+rm -rf linux-installer.sh
+mkdir -p /opt/calibre/library
+cd /opt/calibre/library
+$STD curl -fsSL 'https://github.com/thansen0/sample-epub-minimal/raw/refs/heads/master/minimal.epub' -o minimal.epub
 msg_ok "Installed Calibre"
 
 msg_info "Creating Service"
@@ -35,7 +38,7 @@ After=syslog.target network.target
 [Service]
 UMask=0002
 Type=simple
-ExecStart=/usr/bin/calibre-server --port=8080 --enable-local-write /var/lib/calibre/library
+ExecStart=/opt/calibre/bin/calibre-server --port=8080 --enable-local-write /opt/calibre/library
 TimeoutStopSec=20
 KillMode=process
 Restart=on-failure
